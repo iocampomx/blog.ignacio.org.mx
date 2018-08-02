b60a1821ab52bae6ed99dcad7129fc7cb51f027a
Ignacio Ocampo <ignacio.ocampo@thermofisher.com>
2018-08-02 14:15:23 -0700

    Adding pending files

--- deployS3.sh
+++ deployS3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hugo -v -t kiera
-aws s3 sync --acl "public-read" public/ s3://blog.nafiux.com --exclude 'post' --profile usr-nafiux-iocampo
+aws s3 sync --acl "public-read" public/ s3://blog.ignacio.org.mx --exclude 'post' --profile usr-nafiux-iocampo
--- runWithDrafts.sh
+++ runWithDrafts.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 open http://localhost:1313/
-#hugo server --theme=hugo-theme-minos --buildDrafts
-#hugo server --buildDrafts
 hugo server -t kiera --buildDrafts
